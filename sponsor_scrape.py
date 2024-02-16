9d6a28dd1d3897e7a337407da07a87a5ee436dec
Edward Townsend <ejpt2@cam.ac.uk>
2024-02-16 16:32:32 +0000

    modify+add: add new html for Feb 2024 CUED choice database and sponsor_scrape.py updated

--- sponsor_scrape.py
+++ sponsor_scrape.py
@@ -4,34 +4,13 @@ import pandas as pd
 import os
 import re
 
-with open("london.html", "r", encoding="utf-8") as html_file:
+with open("cued_choice.html", "r", encoding="utf-8") as html_file:
     london_html = html_file.read()
-with open("bedfordshire.html", "r", encoding="utf-8") as html_file:
-    bedfordshire_html = html_file.read()
-with open("berkshire.html", "r", encoding="utf-8") as html_file:
-    berkshire_html = html_file.read()
-with open("cambridgeshire.html", "r", encoding="utf-8") as html_file:
-    cambirdgeshire_html = html_file.read()
-with open("derbyshire.html", "r", encoding="utf-8") as html_file:
-    derbyshire_html = html_file.read()
-with open("gloucs.html", "r", encoding="utf-8") as html_file:
-    gloucs_html = html_file.read()
-with open("hampshire.html", "r", encoding="utf-8") as html_file:
-    hampshire_html = html_file.read()
-with open("kent.html", "r", encoding="utf-8") as html_file:
-    kent_html = html_file.read()
-with open("lancs.html", "r", encoding="utf-8") as html_file:
-    lancs_html = html_file.read()
-with open("northumberland.html", "r", encoding="utf-8") as html_file:
-    northumberland_html = html_file.read()
-with open("uk_orgs.html", "r", encoding="utf-8") as html_file:
-    uk_orgs_html = html_file.read()
 
 def company_parser(html_content, plain_texts):
     soup = BeautifulSoup(html_content, 'html.parser')
     target_attrs = {
-        "target": "_blank",
-        "target": "_new"
+        "target": "_blank"
     } 
     phone_pattern = re.compile(r'Tel:\s*([\d\s\-]+)')
 
@@ -63,11 +42,11 @@ def company_parser(html_content, plain_texts):
 
         plain_texts.append([company_name, person_name_tag, email_address, phone_number])
 
-def remove_strings_with_same_start(list_of_lists):
+def remove_strings_with_same_start(lst_of_lsts):
     processed_words = set()
     result = []
 
-    for lst in list_of_lists:
+    for lst in lst_of_lsts:
         string = lst[0]
         # Check if the string contains more than one word
         if not string.isspace() and len(string.split()) > 0:
@@ -84,22 +63,16 @@ def remove_strings_with_same_start(list_of_lists):
 
 sponsor_list = []
 company_parser(london_html, sponsor_list)
-company_parser(bedfordshire_html, sponsor_list)
-company_parser(berkshire_html, sponsor_list)
-company_parser(cambirdgeshire_html, sponsor_list)
-company_parser(derbyshire_html, sponsor_list)
-company_parser(gloucs_html, sponsor_list)
-company_parser(hampshire_html, sponsor_list)
-company_parser(kent_html, sponsor_list)
-company_parser(lancs_html, sponsor_list)
-company_parser(northumberland_html, sponsor_list)
-company_parser(uk_orgs_html, sponsor_list)
-
+df = pd.read_excel('old_sponsor_list.xlsx', header=None)
+rows_list = df.where(pd.notna(df), None).values.tolist()
+sponsor_list = sponsor_list + rows_list
 sponsor_list = remove_strings_with_same_start(sponsor_list)
+print(len(sponsor_list))
 
 company_name_email = []
 company_email = []
 company = []
+
 for lst in sponsor_list:
     if lst[2] != None:
         if lst[1] != "":
@@ -110,15 +83,19 @@ for lst in sponsor_list:
         company.append(lst)
 
 dir_path = os.getcwd()
+
 xlsx_path_1 = os.path.join(dir_path, "companies.xlsx")
-xlsx_path_2 = os.path.join(dir_path, "companies_emails.xlsx")
-xlsx_path_3 = os.path.join(dir_path, "companies_names_emails.xlsx")
 df1 = pd.DataFrame(company)
 df1.to_excel(xlsx_path_1, index=False, header=False)
+
+xlsx_path_2 = os.path.join(dir_path, "companies_emails.xlsx")
 df2 = pd.DataFrame(company_email)
 df2.to_excel(xlsx_path_2, index=False, header=False)
+
+xlsx_path_3 = os.path.join(dir_path, "companies_names_emails.xlsx")
 df3 = pd.DataFrame(company_name_email)
 df3.to_excel(xlsx_path_3, index=False, header=False)
+
 print(len(company))
 print(len(company_email))
 print(len(company_name_email))
